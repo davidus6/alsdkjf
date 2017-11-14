f9e0de2e1a8349a0ffebd4ab669b4f577e21ba3f
davidus6 <davejane@seznam.cz>
2017-11-14 11:03:06 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,110 @@
+<html>
+	<head>
+		<meta charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<title>IIS proj</title>
+		<link href="http://fonts.googleapis.com/css?family=Ubuntu+Condensed" rel="stylesheet" type="text/css" />
+		<link href="http://fonts.googleapis.com/css?family=Kreon" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="header">
+				<div id="logo">
+					<h1><a href="#">Hudební ráj</a></h1>
+				</div>
+				<div id="menu">
+					<ul>
+						<li class="first current_page_item"><a href="#">Domù</a></li>
+						<li><a href="#">Umìlci</a></li>
+						<li><a href="#">Události</a></li>
+						<li class="last"><a href="#">Kontakt</a></li>
+					</ul>
+					<br class="clearfix" />
+				</div>
+			</div>
+			<div id="splash">
+				<img src="images/pic1.jpeg" width="1180" height="295" alt="" />
+			</div>
+			<div id="page">
+				<div id="content">
+					<div class="box">
+						<h2>Welcome to Artifice</h2>
+						<p>
+							blblbl
+						</p>
+					</div>
+					<div id="col1" class="box">
+						<h3>Suscipit sed pellentesque nascetur</h3>
+						<ul class="section-list">
+							<li class="first">
+							<img class="alignleft" src="images/pic2.jpg" width="60" height="60" alt="" /> <span>Lacinia natoque faucibus fringilla egestas.</span></li>
+							<li class="last">
+							<img class="alignleft" src="images/pic3.jpg" width="60" height="60" alt="" /> <span>Penatibus quam auctor tristique aenean et lorem ipsum dolor et sit amet nisl veroeros et consequat amet lorem quisque duis lorem.</span></li>
+						</ul>
+					</div>
+					<div id="col2" class="box">
+						<h3>Penatibus amet sed consequat accumsan</h3>
+						<img class="aligntop" src="images/pic4.jpg" width="195" height="100" alt="" />
+						<p>
+							Parturient imperdiet dignissim aliquet duis pulvinar. Vulputate curae ipsum eu feugiat quisque duis. Magnis augue urna dolore aliquam integer enim lorem ipsum dolor sit amet veroeros penatibus et consequat.
+						</p>
+					</div>
+					<br class="clearfix" />
+				</div>
+				<div id="sidebar2">
+					<div class="box">
+						<h3>Volutpat condimentum auctor magna</h3>
+						<p>
+							Rhoncus et dui dignissim elementum. Vivamus lorem eu nisl phasellus lorem dolore.
+						</p>
+						<ul class="list">
+							<li class="first"><a href="#">Mollis ornare arcu suscipit</a></li>
+							<li><a href="#">Accumsan quam ante</a></li>
+							<li><a href="#">Volutpat odio neque posuere</a></li>
+							<li class="last"><a href="#">Volutpat urna tristique quisque</a></li>
+						</ul>
+					</div>
+					<div class="box">
+						<h3>Cubilia turpis</h3>
+						<ul class="list">
+							<li class="first"><a href="#">Libero duis ultrices</a></li>
+							<li><a href="#">Mattis quam massa mollis</a></li>
+							<li><a href="#">Nibh nisi blandit dolor tellus</a></li>
+							<li><a href="#">Nulla nulla dolore cras</a></li>
+							<li class="last"><a href="#">Montes convallis consectetur</a></li>
+						</ul>
+					</div>
+				</div>
+				<div id="sidebar1">
+					<div class="box">
+						<h3>Fringilla pharetra quis</h3>
+						<p>
+							Phasellus condimentum quis nec suscipit vel phasellus rutrum. Tincidunt posuere eu integer risus magna lorem ipsum veroeros.
+						</p>
+					</div>
+					<div class="box">
+						<h3>Volutpat condimentum auctor dolore</h3>
+						<p>
+							Sapien ullamcorper mus penatibus viverra curae erat consectetur. Scelerisque eleifend quis ullamcorper ante laoreet scelerisque.
+						</p>
+						<ul class="list">
+							<li class="first"><a href="#">Quis pellentesque orci cras</a></li>
+							<li><a href="#">Fringilla laoreet ultrices rhoncus</a></li>
+							<li><a href="#">Elementum primis viverra</a></li>
+							<li><a href="#">Curae mus penatibus et lorem</a></li>
+							<li><a href="#">Lorem ipsum dolor sit amet</a></li>
+							<li><a href="#">Ornare convallis laoreet</a></li>
+							<li class="last"><a href="#">Elementum ante posuere magna</a></li>
+						</ul>
+					</div>
+				</div>
+				<br class="clearfix" />
+			</div>
+		</div>
+		<div id="footer">
+			&copy; 2012 Untitled | Design by <a href="http://www.freecsstemplates.org/">FCT</a>
+		</div>
+	</body>
+</html>
